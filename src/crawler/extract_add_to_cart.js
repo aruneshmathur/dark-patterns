e0c36be536a3d2ec7848ec3df4353f2eb7f7fc8a
Michael J Friedman <mfred240@gmail.com>
2018-12-02 18:20:39 -0500

    Added button color as a factor in classifying add-to-cart buttons
    Measure of "distance" between button color and page background
    color helps to distinguish primary buttons (which are more likely
    to be add-to-cart) from secondary buttons. This is taken into
    account when ranking buttons in the classifier.

--- src/crawler/extract_add_to_cart.js
+++ src/crawler/extract_add_to_cart.js
@@ -11,6 +11,27 @@ var debug = function(msg) {
     }
 };
 
+// Gets an element given its string xpath
+var getElementByXpath = function(path) {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+};
+
+// Returns an array of the three rgb color values, given a string of the form
+// "rgb(#, #, #)" or "rgba(#, #, #, #)".
+var extractRgb = function(str) {
+    let matchesRgb = str.match(/^rgb\((\d+),\s*(\d+),\s*(\d+)\)$/);
+    let matchesRgba = str.match(/^rgba\((\d+),\s*(\d+),\s*(\d+),\s*([0-9.]+)\)$/);
+    let rgb;
+    if (matchesRgb != null) {
+        rgb = [parseInt(matchesRgb[1]), parseInt(matchesRgb[2]), parseInt(matchesRgb[3])];
+    } else if (matchesRgba != null) {
+        rgb = [parseInt(matchesRgba[1]), parseInt(matchesRgba[2]), parseInt(matchesRgba[3])];
+    } else {
+        console.error("Invalid rgb string: " + rgb);
+    }
+    return rgb;
+};
+
 // Returns true if any attribute of elem matches the regex.
 var anyAttributeMatches = function(elem, regex) {
     for (let i = 0; i < elem.attributes.length; i++) {
@@ -18,6 +39,28 @@ var anyAttributeMatches = function(elem, regex) {
             return true;
         }
     }
+};
+
+// Returns the absolute difference between this element's color and the page's
+// background color (normalized so max distance is 1).
+var computeBackgroundColorDistance = function(elem) {
+    let elemRgbStr = window.getComputedStyle(elem).backgroundColor;
+    let body = document.getElementsByTagName("body");
+    if (body.length == 0) {
+        debug("No body tag, unable to determine background color");
+        return 0;
+    }
+    body = body[0];
+    let bodyRgbStr = window.getComputedStyle(body).backgroundColor;
+
+    let elemRgb = extractRgb(elemRgbStr);
+    let bodyRgb = extractRgb(bodyRgbStr);
+    let dist = 0;
+    for (let i = 0; i < 3; i++) {
+        dist += (elemRgb[i] - bodyRgb[i]) * (elemRgb[i] - bodyRgb[i]);
+    }
+    dist = Math.sqrt(dist) / Math.sqrt(3*255*255); // normalize so max is 1
+    return dist;
 };
 
 // Runs various heuristics for identifying the add-to-cart button on the given
@@ -45,36 +88,38 @@ var computeIndividualScore = function(elem) {
         return 0;
     }
 
-    let regex = "[Aa][Dd][Dd][ ]{0,1}[Tt][Oo].*"; // variants of "add to cart"
-    let score = 0;
+    // Scores for each feature. The "score" field is out of 1, and the weight
+    // is a multiplier for the score.
+    let scores = {
+        regex: {score: 0, weight: 3},
+        colorDist: {score: 0, weight: 1}
+    };
 
-    // Text says a variant of "add to _"
+    // Text or any attribute contains a variant of "add to _"
+    let regex = "[Aa][Dd][Dd][ ]{0,1}[Tt][Oo].*"; // variants of "add to cart"
     if (elem.textContent.match(regex) != null) {
         debug("isAddToCart: text matches regex");
-        score += 1;
-    }
-
-    // Any attribute contains a variant of "add to _"
-    if (anyAttributeMatches(elem, regex)) {
+        scores.regex.score = 1;
+    } else if (anyAttributeMatches(elem, regex)) {
         debug("isAddToCart: attribute matches regex");
-        score += 1;
-    }
-
-    // Parent div/elem has attribute that contains a variant of "add to _"
-    if (anyAttributeMatches(elem.parentElement, regex)) {
+        scores.regex.score = 1;
+    } else if (anyAttributeMatches(elem.parentElement, regex)) {
         debug("isAddToCart: parent element attribute matches regex");
-        score += 1;
+        scores.regex.score = 1;
+    } else if (elem["src"] != undefined && elem["src"].match(regex) != null) {
+        debug("isAddToCart: img src matches regex");
+        scores.regex.score = 1;
     }
 
-    // For images, check if src contains a variant of "add to _"
-    imgSrc = "src";
-    if (elem[imgSrc] != undefined) {
-        if (elem[imgSrc].match(regex) != null) {
-            debug("isAddToCart: img src matches regex");
-            score += 1;
-        }
-    }
+    // Add the "distance" between this element's color and its surrounding
+    // background color - the more different the colors, the higher the score
+    scores.colorDist.score = computeBackgroundColorDistance(elem);
 
+    // Compute total score
+    let score = 0;
+    Object.keys(scores).forEach((ft, _) => {
+        score += scores[ft].weight * scores[ft].score;
+    });
     return score;
 };
 
@@ -83,6 +128,7 @@ var computeIndividualScore = function(elem) {
 // if no possible buttons are found.
 var getPossibleAddToCartButtons = function() {
     let candidates = [];
+    let scoreThresh = 1;
 
     // Select elements that could be buttons, and compute their scores
     possibleTags.forEach(tag => {
@@ -90,7 +136,7 @@ var getPossibleAddToCartButtons = function() {
         for (let i = 0; i < matches.length; i++) {
             let elem = matches[i];
             let score = computeIndividualScore(elem);
-            if (score > 0) {
+            if (score > scoreThresh) {
                 candidates.push({
                     element: elem,
                     score: score
@@ -102,8 +148,8 @@ var getPossibleAddToCartButtons = function() {
     // Sort by score
     // Return top element with a score above 0, if there is one
     candidates.sort(function(x, y) {
-        if (x.score < y.score) return -1;
-        if (x.score > y.score) return 1;
+        if (x.score < y.score) return 1;
+        if (x.score > y.score) return -1;
         return 0;
     });
 
