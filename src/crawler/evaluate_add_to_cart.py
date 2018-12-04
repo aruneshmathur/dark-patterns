2e65f54add13023eb581c9f0c6806c7749f7deb7
Michael J Friedman <mfred240@gmail.com>
2018-12-04 00:53:01 -0500

    Added to evaluation script to write results to file

--- src/crawler/evaluate_add_to_cart.py
+++ src/crawler/evaluate_add_to_cart.py
@@ -4,17 +4,20 @@ import os
 import sys
 import time
 
-usage = 'Usage: python %s DATA-FILE' % os.path.basename(__file__)
+usage = 'Usage: python %s DATAFILE OUTFILE' % os.path.basename(__file__)
 
 # Run evaluation on the dataset provided as arg. Dataset should be a csv file
-# with two columns: url (string), add to cart element (string)
+# with two columns: url (string), add-to-cart element (string). Output is a csv
+# a copy of the data file, with an additional column for the add-to-cart
+# element found by the classifier.
 def main():
-  if len(sys.argv[1:]) < 1:
+  if len(sys.argv[1:]) < 2:
     print usage
     exit(1)
 
   # Import data
   data_filename = sys.argv[1]
+  out_filename = sys.argv[2]
   urls = []
   elements = []
   with open(data_filename, 'r') as f:
@@ -25,21 +28,51 @@ def main():
 
   # Run evaluation
   print 'Launching browser...'
-  driver = webdriver.Firefox(executable_path=r'/usr/local/bin/geckodriver')
+  options = webdriver.firefox.options.Options()
+  options.headless = True
+  driver = webdriver.Firefox(options=options, executable_path=r'/usr/local/bin/geckodriver')
   print 'Running evaluation...'
   t0 = time.time()
-  accuracy = evaluate(driver, urls, elements)
+  num_correct = 0
+  num_false_pos = 0
+  num_false_neg = 0
+  total_pos = 0
+  total_neg = 0
+  output = []
+  with open(out_filename, 'w') as f:
+    writer = csv.writer(f)
+    for i in range(len(urls)):
+      prediction = extract_add_to_cart(driver, urls[i])
+      prediction = prediction.replace('\n', '').replace('\r\n', '') # strip newlines
+      output.append([urls[i], elements[i], prediction])
+
+      if i % 10 == 0 or i == len(urls)-1:
+        writer.writerows(output)
+        output = []
+
+      if elements[i] == '':
+        total_neg += 1
+      else:
+        total_pos += 1
+
+      if prediction == elements[i]:
+        num_correct += 1
+      elif elements[i] == '':
+        num_false_pos += 1
+      else:
+        num_false_neg += 1
+  accuracy = float(num_correct) / float(len(elements))
+  false_pos = float(num_false_pos) / float(total_pos)
+  false_neg = float(num_false_neg) / float(total_neg)
   elapsed = time.time() - t0
   driver.close()
   print 'Done (%ds)' % int(elapsed)
-  print 'Accuracy = %.2f' % accuracy
 
-# Evaluates the accuracy of the add-to-cart classifier, given the list of urls
-# and the list element strings (the corresponding add-to-cart button for each
-# url), and a web driver. Returns the fraction of correct add-to-cart buttons
-# found.
-def evaluate(driver, urls, elements):
-  return float(sum([extract_add_to_cart(driver, url) == element for url, element in zip(urls, elements)])) / float(len(elements))
+  print '\nRESULTS'
+  print 'Raw results are written to %s' % out_filename
+  print 'Accuracy = %.2f (%d/%d)' % (accuracy, num_correct, len(elements))
+  print 'False positive rate = %.2f (%d/%d)' % (false_pos, num_false_pos, total_pos)
+  print 'False negative rate = %.2f (%d/%d)' % (false_neg, num_false_neg, total_neg)
 
 # Extracts the add-to-cart button from the url using the corresponding JS
 # script, given a web driver. Returns the button element as a string (outer
