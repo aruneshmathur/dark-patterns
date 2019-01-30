0927d92a8b5f686725a530c581dd593f4baf4ce0
Michael J Friedman <mfred240@gmail.com>
2019-01-31 00:49:14 +0100

    Combined preprocessing and feature transformation
    The time spent writing and reading data to/from files between
    preprocessing and feature transformation was a bottleneck in
    data analysis, so these steps are now combined to save the time.

--- analysis/clustering/clustering.sh
+++ analysis/clustering/clustering.sh
@@ -1,5 +1,6 @@
+#!/bin/bash
+db=$1
 source /n/fs/darkpatterns/analysis/dp/bin/activate && \
-  python preprocessing.py $1 segments.dataframe && \
-  python feature_transformation.py segments.dataframe features.npy segments_feature.dataframe && \
-  python clustering.py features.npy segments_feature.dataframe segments_cluster.dataframe && \
+  python preprocessing.py $db features.npy segments.dataframe && \
+  python clustering.py features.npy segments.dataframe segments_cluster.dataframe && \
   python visualize.py segments_cluster.dataframe clusters.csv
--- analysis/clustering/preprocessing.py
+++ analysis/clustering/preprocessing.py
@@ -1,13 +1,22 @@
-import sys
 import logging
+import nltk
+from nltk.stem.porter import PorterStemmer
+import numpy as np
 import os
-import sqlite3
 import pandas as pd
+from scipy.sparse import hstack
+from scipy import sparse
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+from sklearn.preprocessing import normalize
+import spacy
+import sqlite3
+import sys
 from urlparse import urlparse
 
-usage = 'python %s DB OUTFILE' % __file__
-help_message = '''Produces a pandas dataframe with containing the segments read from
-the DB. Dataframe is written to OUTFILE.'''
+usage = 'python %s DB FT-OUTFILE SEGMENTS-OUTFILE' % __file__
+help_message = '''Computes feature vectors from the segment data read from the DB.
+Produces an array of feature vectors to FT-OUTFILE, and a new segments dataframe written
+to SEGMENTS-OUTFILE.'''
 LOG_FILE_NAME = 'preprocessing.log'
 
 try:
@@ -22,16 +31,54 @@ lf_handler.setFormatter(lf_format)
 logger.addHandler(lf_handler)
 logger.setLevel(logging.INFO)
 
+stemmer = PorterStemmer()
+stopwords = nltk.corpus.stopwords.words('english')
+
+def tokenize(line):
+    if (line is None):
+        line = ''
+    tokens = [stemmer.stem(t) for t in nltk.word_tokenize(line) if len(t) != 0 and t not in stopwords and not t.isdigit()]
+    return tokens
+
+def get_count_features(column, binary_rep):
+    logger.info('Using CountVectorizer with binary=%s' % str(binary_rep))
+    vec = CountVectorizer(tokenizer=tokenize, binary=binary_rep, strip_accents='ascii').fit(column)
+    logger.info('Length of vocabulary %s' % str(len(vec.vocabulary_)))
+    vec = vec.transform(column)
+    return normalize(vec, axis=0)
+
+def get_tfidf_features(column, binary_rep):
+    logger.info('Using TfidfVectorizer with binary=%s' % str(binary_rep))
+    vec = TfidfVectorizer(tokenizer=tokenize, binary=binary_rep, strip_accents='ascii').fit(column)
+    logger.info('Length of vocabulary %s' % str(len(vec.vocabulary_)))
+    return vec.transform(column)
+
+def get_word_vector_features(column):
+    nlp = spacy.load('en_core_web_lg')
+    vecs = []
+
+    logger.info('Using word vectors')
+    for doc in nlp.pipe(column.str.replace(r'\d+', '').astype('unicode').values, batch_size=10000, n_threads=7):
+        if doc.is_parsed:
+            vecs.append(doc.vector)
+        else:
+            vecs.append(None)
+
+    return np.array(vecs)
+
 if __name__ == '__main__':
     # Check args
-    if len(sys.argv[1:]) != 2:
+    if len(sys.argv[1:]) != 3:
         print usage
         print ''
         print help_message
         exit(1)
+    db = sys.argv[1]
+    features_outfile = sys.argv[2]
+    segments_outfile = sys.argv[3]
+
+    # Read segment data from database
     try:
-        db = sys.argv[1]
-        outfile = sys.argv[2]
         con = sqlite3.connect(db)
 
         # Analyzing websites visited
@@ -80,10 +127,44 @@ if __name__ == '__main__':
 
         logger.info('segments[\'newline_count\'].describe(): \n %s' % segments['newline_count'].describe().to_string())
         logger.info('segments[\'inner_text_length\'].describe(): \n %s' % segments['inner_text_length'].describe().to_string())
+    except:
+        logger.exception('Exception')
 
-        logger.info('Pickling segments ...')
-        segments.to_pickle(outfile)
+    # Create feature vectors from data
+    try:
+        logger.info('Replacing numbers with DPNUM placeholder ...')
+        segments['inner_text_processed'] = segments['inner_text'].str.replace(r'\d+', 'DPNUM')
+        segments['longest_text_processed']= segments['longest_text'].str.replace(r'\d+', 'DPNUM')
         logger.info('Done')
 
+        logger.info('Removing redundant nodes ...')
+        segments = segments.groupby(['domain']).apply(lambda x: x.drop_duplicates(subset=['inner_text_processed'], keep='last'))
+        logger.info('Done')
+
+        logger.info('Number of segments: %s' % str(segments.shape))
+        logger.info('segments columns: %s' % str(list(segments.columns.values)))
+
+        logger.info('Creating the feature representation ...')
+        #features = get_count_features(segments['inner_text'], True)
+        features = get_count_features(segments['inner_text'], False)
+        #features = get_tfidf_features(segments['inner_text'], True)
+        #features = get_tfidf_features(segments['inner_text'], False)
+        #features = get_word_vector_features(segments['inner_text'])
+        logger.info('Done')
+
+        logger.info('Number of features: %s' % str(features.shape))
+
+        logger.info('Pickling features ...')
+
+        if (sparse.issparse(features)):
+            features = features.toarray()
+
+        np.save(features_outfile, features)
+
+        logger.info('Done')
+
+        logger.info('Pickling feature processed segments ...')
+        segments.to_pickle(segments_outfile)
+        logger.info('Done')
     except:
         logger.exception('Exception')
