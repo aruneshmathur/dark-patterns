2001e2d032e5f781ab76ad65834da54df1276d92
Gunes Acar <gunes.acar@esat.kuleuven.be>
2019-02-26 02:21:44 -0500

    Fix mkdir option.

--- analysis/timer-analysis/slide-show.sh
+++ analysis/timer-analysis/slide-show.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mkdir -P ~/timer-slideshow
+mkdir -p ~/timer-slideshow
 ffmpeg -framerate 5  -pattern_type glob -i '~/stateful_countdown_crawl_*/output/*/*marked.png' -g 1 -c:v libx264 ~/timer-slideshow/timer.mp4
