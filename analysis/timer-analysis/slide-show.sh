ee175e7de02f0a6863a2035cc9c19c0e33933d08
Gunes Acar <gunes.acar@esat.kuleuven.be>
2019-02-26 02:20:24 -0500

    Create slideshow dir, if it doesn't exist.o

--- analysis/timer-analysis/slide-show.sh
+++ analysis/timer-analysis/slide-show.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-ffmpeg -framerate 5  -pattern_type glob -i '~/stateful_countdown_crawl_*/output/*/*marked.png' -g 1 -c:v libx264 ~/timer-videos/timer.mp4
+mkdir -P ~/timer-slideshow
+ffmpeg -framerate 5  -pattern_type glob -i '~/stateful_countdown_crawl_*/output/*/*marked.png' -g 1 -c:v libx264 ~/timer-slideshow/timer.mp4
