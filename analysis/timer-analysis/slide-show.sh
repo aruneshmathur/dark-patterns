cf54b48e2ee24c961058b55fc093a6223ef16c49
Gunes Acar <gunes.acar@esat.kuleuven.be>
2019-03-27 16:00:39 -0400

    Use odin paths.

--- analysis/timer-analysis/slide-show.sh
+++ analysis/timer-analysis/slide-show.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 python add_timestamp_to_screenshots.py
-cd ~
+cd /mnt/10tb4/dp-crawls/
 mkdir -p timer-slideshow
-ffmpeg -y -framerate 5  -pattern_type glob -i 'stateful_countdown_crawl_*/output/*/*marked.png' -g 1 -c:v libx264 ~/timer-slideshow/timer.mp4
+ffmpeg -y -framerate 5  -pattern_type glob -i 'stateful_countdown_crawl_*/output/*/*marked.png' -g 1 -c:v libx264 /mnt/ssd/timer-slideshow/timer.mp4
+chmod o+r /mnt/ssd/timer-slideshow/timer.mp4
+scp /mnt/ssd/timer-slideshow/timer.mp4 macar@portal.cs.princeton.edu:/n/fs/darkpatterns/www/
